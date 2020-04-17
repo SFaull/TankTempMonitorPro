7336b99d802241bb23660706d4d1b563effdc664
Sam Faull <samz5991@hotmail.co.uk>
2020-04-17 14:46:56 +0100

    Removed delays from init routine that are not required

--- firmware/TankTempMonitorPro/TankTempMonitorPro.ino
+++ firmware/TankTempMonitorPro/TankTempMonitorPro.ino
@@ -57,7 +57,6 @@ void setup()
                     1,           /* priority of the task */
                     &Task1,      /* Task handle to keep track of created task */
                     1);          /* pin task to core 0 */                  
-  delay(500); 
 
   //create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
   xTaskCreatePinnedToCore(
@@ -68,7 +67,6 @@ void setup()
                     1,           /* priority of the task */
                     &Task2,      /* Task handle to keep track of created task */
                     0);          /* pin task to core 1 */
-    delay(500); 
     
 }
 
